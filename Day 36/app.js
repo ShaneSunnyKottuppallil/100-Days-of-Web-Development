e58968d20d9fa7995d11a76660d91c09b9f16a2b
Shane Sunny Kottuppallil <shanesunnyssk@gmail.com>
2025-02-16 23:39:55 +0530

    Commit

--- Day 36/app.js
+++ Day 36/app.js
@@ -12,3 +12,33 @@ const student={
 };
 
 console.log(student.getAvg());
+console.log(this);
+
+// TRY AND CATCH 
+try{
+    console.log(a);
+}catch(e){
+    console.log("Error! a is not defined by shane");
+    console.log("Printing Error\n",e);
+}
+
+// Arrow Functions 
+const sum =(a,b)=>{
+    console.log("Sum using Arrow Funtion is :",a+b);
+}
+sum(2,9);
+
+const squarearea= (a)=>{
+    console.log("Area of square with Side",a,"is :",a*a);
+}
+squarearea(15);
+
+const wish= () =>{
+    console.log("\nHello")
+}
+wish()
+
+const mult=(a,b)=>(
+    a*b
+);
+console.log(mult(4,9));
---
+++ Day 37/app.js
@@ -0,0 +1,64 @@
+// setTimeout 
+console.log("Hello!");
+const nameof=()=>{
+    console.log("Shane Sunny Kottuppallil");
+}
+setTimeout(nameof,2500);
+
+const msg=()=>{
+    console.log("I will come ,wait for 2s and I will come  again.");
+}
+// let id=setInterval(msg,3000);
+console.log("Welcome to");
+
+// To stop printing msg (To stop setInterval)
+// clearInterval(id);
+
+
+// "this" keyword in Normal Funtion and Arrow function 
+//Normal Function
+const student={
+    name:"shane",
+    marks:90,
+    printline:function(){
+        console.log(this.marks);
+        console.log("this=",this);
+    }
+};
+student.printline();
+
+//Arrow Funtion
+const student2={
+    name2:"Sven",
+    marks2:"100",
+    printline2:()=>{
+        console.log(this.marks2,"for 2nd fucntion");
+        console.log("for 2nd fucntion this=",this);
+    }
+};
+student2.printline2();
+
+
+
+// A better Example 
+const student3={
+    name:"Shane Sunny",
+    marks:99,
+    //First- arrow function inside a normal function
+    getinfo1:function(){
+        setTimeout(()=>{
+            console.log("This of getinfo1 (Arrow Function)",this);//Student  ( Parent is function() whose scope is object (student3))
+        })
+    },
+    //Second- normal function inside a normal function
+    getinfo2:function(){
+        setTimeout(function (){
+            console.log("This of getinfo2 (Normal Function)",this);//Window (Scope is same as Parents scope (here parent is setTimeout ))
+        })
+    }
+}
+
+student3.getinfo1();
+student3.getinfo2();
+
+
