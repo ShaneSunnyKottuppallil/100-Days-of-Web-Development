6d6e4365bcedc4710a0daf5ae1df6ef89e78ceb9
Shane Sunny Kottuppallil <shanesunnyssk@gmail.com>
2025-02-26 18:33:51 +0530

    Commit

--- Day 40/JS DOM/app.js
+++ Day 40/JS DOM/app.js
@@ -16,3 +16,11 @@ console.log("\n");
 // Get Element by Tag Name 
 ptag=document.getElementsByTagName("p");
 console.dir(ptag);
+console.log("\n");
+
+
+// Query Selectors in JS 
+console.dir(document.querySelector('h1'));
+console.dir(document.querySelector('#mainImg'));
+console.dir(document.querySelector('.oldImg'));
+console.dir(document.querySelectorAll('.oldImg'));
